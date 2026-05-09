a08579d0f7c26a560c9c5001ca85f5145044ff78
rumilityru <rumilityru@gmail.com>
2026-05-09 13:57:42 +0800

    Add files via upload

---
+++ FindingCircumference.py
@@ -0,0 +1,12 @@
+#Finding Circumference of Circle:
+#C=2πr
+
+import math
+radius = float(input('Enter RADIUS: '))
+diameter = radius * 2
+Circumference = math.pi * diameter
+print(Circumference)
+
+
+
+
